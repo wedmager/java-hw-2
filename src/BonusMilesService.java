48297a6ddc220396ea2e5711671a3a63721997ee
Marat Gaptulazyanov <gap.marat@gmail.com>
2025-05-04 11:20:55 +0400

    Update Main + add BonusMilesService

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+
+    public int calculate(int cost) {
+        int rublesPerMile = 20;
+        return cost / rublesPerMile;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
+
     public static void main(String[] args) {
-
-        int ticketPrice = 1500; // Стоимость билета в рублях
-        int rublesPerMile = 20; // За сколько рублей даётся одна миля
-
-        int milesEarned = ticketPrice / rublesPerMile;
-
-        System.out.println("Начислено бонусных миль: " + milesEarned);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println("Начислено бонусных миль: " + miles);
     }
 }
